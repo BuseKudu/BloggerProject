01e44485e5a5da97b7c892a1ac88ef0454ddd591
BuseKudu <94551478+BuseKudu@users.noreply.github.com>
2022-04-09 03:21:08 +0300

    Business Layer Starting...

---
+++ DataAccessLayer/Migrations/202204012203096_one.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class one : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Contents", "WriterID", "dbo.Writers");
+            DropIndex("dbo.Contents", new[] { "WriterID" });
+            DropColumn("dbo.Contents", "WriterID");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Contents", "WriterID", c => c.Int());
+            CreateIndex("dbo.Contents", "WriterID");
+            AddForeignKey("dbo.Contents", "WriterID", "dbo.Writers", "WriterID");
+        }
+    }
+}
